b5116876bf233ff594e050b5b6169283e0c2347e
tramon <andriitramon@gmail.com>
2025-03-12 11:02:44 +0200

    L-2.2 fixed vertex

--- lab2/Manipulate3D.py
+++ lab2/Manipulate3D.py
@@ -10,7 +10,7 @@ class Manipulate3D:
             (0, 1, 2),  # left
             (0, 1, 3),  # bottom
             (1, 2, 3),  # right
-            (2, 0, 3)   # back
+            (2, 0, 3)  # back
         ]
 
     @staticmethod
@@ -30,24 +30,6 @@ class Manipulate3D:
         return point_adjusted_to_real_center
 
     @staticmethod
-    def rotate_y_fixed_top_center(points, angle):
-        rad = math.radians(angle)
-        tetrahedron_center = np.mean(points)
-        top_vertex_initial = points[2].copy()  # fixate top vertex
-
-        translated = points - tetrahedron_center
-        matrix = np.array([
-            [math.cos(rad), 0, math.sin(rad)],
-            [0, 1, 0],
-            [-math.sin(rad), 0, math.cos(rad)]
-        ])
-
-        rotated = np.dot(translated, matrix.T) + tetrahedron_center
-        rotated[2] = top_vertex_initial  # get fixated top vertex and overwrite
-
-        return rotated
-
-    @staticmethod
     def convert_coordinates_to_points(points, center_x, center_y):
         windows_points = []
         for p in points:
@@ -55,9 +37,50 @@ class Manipulate3D:
         return windows_points
 
     @staticmethod
-    def undraw_faces(face_objects):
-        for face in face_objects:
-            face.undraw()
+    def axis_matrix_operations(points, angle, tetrahedron_center, axis='y'):
+        rad = math.radians(angle)
+        if axis == 'x':
+            adjusted_points = points - tetrahedron_center
+            matrix = np.array([
+                [1, 0, 0],
+                [0, math.cos(rad), -math.sin(rad)],
+                [0, math.sin(rad), math.cos(rad)]
+            ])
+            rotated_points = np.dot(adjusted_points, matrix.T) + tetrahedron_center
+        elif axis == 'y':
+            adjusted_points = points - tetrahedron_center
+            matrix = np.array([
+                [math.cos(rad), 0, math.sin(rad)],
+                [0, 1, 0],
+                [-math.sin(rad), 0, math.cos(rad)]
+            ])
+            rotated_points = np.dot(adjusted_points, matrix.T) + tetrahedron_center
+        elif axis == 'z':
+            rad = math.radians(angle)
+            adjusted_points = points - tetrahedron_center
+            matrix = np.array([
+                [math.cos(rad), -math.sin(rad), 0],
+                [math.sin(rad), math.cos(rad), 0],
+                [0, 0, 1]
+            ])
+            rotated_points = np.dot(adjusted_points, matrix.T) + tetrahedron_center
+        else:
+            raise ValueError("Invalid axis. Choose from 'x', 'y', or 'z'.")
+        return rotated_points
+
+    @staticmethod
+    def rotate(points, angle, axis):
+        tetrahedron_center = np.mean(points)
+        rotated_points = Manipulate3D.axis_matrix_operations(points, angle, tetrahedron_center, axis)
+
+        return rotated_points
+
+    @staticmethod
+    def rotate_fixed_vertex(points, angle, axis):
+        tetrahedron_center = np.mean(points[:3], axis=0)
+        rotated_points = Manipulate3D.axis_matrix_operations(points, angle, tetrahedron_center, axis)
+
+        return rotated_points
 
     def draw_faces(self, window, window_points, colors):
         face_objects = []
@@ -71,39 +94,56 @@ class Manipulate3D:
             face_objects.append(tetrahedron_face)
         return face_objects
 
+    @staticmethod
+    def undraw_faces(face_objects):
+        for face in face_objects:
+            face.undraw()
+
     def draw_tetrahedron(self, win, center_x, center_y, size, colors):
         points = self.prepare_tetrahedron_points(size)
         windows_points = self.convert_coordinates_to_points(points, center_x, center_y)
-
         face_objects = self.draw_faces(win, windows_points, colors)
 
         win.getMouse()
-
         Manipulate3D.undraw_faces(face_objects)
 
-    def animate_tetrahedron_rotation(self, win, center_x, center_y, angle, colors):
+    def animate_rotation(self, win, center_x, center_y, size, angle, colors, axis):
         points = Manipulate3D.prepare_tetrahedron_points(size)
-        rotated_points = Manipulate3D.rotate_y_fixed_top_center(points, angle)
-
+        rotated_points = Manipulate3D.rotate(points, angle, axis)
         windows_points = self.convert_coordinates_to_points(rotated_points, center_x, center_y)
-
         face_objects = self.draw_faces(win, windows_points, colors)
-
         return face_objects
 
-    def infinite_rotation(self, window, center_x, center_y, colors):
+    def animate_rotation_fixed_vertex(self, win, center_x, center_y, size, angle, colors, axis):
+        points = Manipulate3D.prepare_tetrahedron_points(size)
+        rotated_points = Manipulate3D.rotate_fixed_vertex(points, angle, axis)
+        windows_points = self.convert_coordinates_to_points(rotated_points, center_x, center_y)
+        face_objects = self.draw_faces(win, windows_points, colors)
+        return face_objects
+
+    def infinite_animation(self, window, center_x, center_y, size, colors, axis):
         angle = 0
         while True and window.checkMouse() is None:
-            tetrahedron = self.animate_tetrahedron_rotation(window, center_x, center_y, angle, colors)
-            time.sleep(0.15)
+            tetrahedron = self.animate_rotation(window, center_x, center_y, size, angle, colors, axis)
+            time.sleep(0.05)
             for face in tetrahedron:
                 face.undraw()
 
-            angle += 5
+            angle += 2
+
+    def infinite_animation_fixed_vertex(self, window, center_x, center_y, size, colors, axis):
+        angle = 0
+        while True and window.checkMouse() is None:
+            tetrahedron = self.animate_rotation_fixed_vertex(window, center_x, center_y, size, angle, colors, axis)
+            time.sleep(0.05)
+            for face in tetrahedron:
+                face.undraw()
+
+            angle += 2
 
 
 if __name__ == "__main__":
-    window = GraphWin("Task - 2: 3D projection of Tetrahedron", 1000, 1000)
+    window = GraphWin("Task - 2: 3D projection of Tetrahedron rotations", 1000, 1000)
     window.setCoords(0, 0, 1000, 1000)
     window.setBackground('white')
 
@@ -112,8 +152,10 @@ if __name__ == "__main__":
     center_x = 500
     center_y = 500
     size = 350
-    colors = ['', 'cyan', 'orange', '']
+    colors = ['', 'orange', 'cyan', '']
 
     manipulate3d.draw_tetrahedron(window, center_x, center_y, size, colors)
 
-    manipulate3d.infinite_rotation(window, center_x, center_y, colors)
+    manipulate3d.infinite_animation(window, center_x, center_y, size, colors, axis='y')
+
+    manipulate3d.infinite_animation_fixed_vertex(window, center_x, center_y, size, colors, axis='z')
